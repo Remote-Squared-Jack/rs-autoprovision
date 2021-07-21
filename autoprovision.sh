bc36d11bdfcc1af975d5ba4ed04c5f0fcd740715
Remote-Squared-Jack <84894090+Remote-Squared-Jack@users.noreply.github.com>
2021-07-21 14:10:34 -0700

    Update autoprovision.sh

--- autoprovision.sh
+++ autoprovision.sh
@@ -16,7 +16,7 @@ sudo apt-get install xdotool -y
 # <----------------------------------------- Openbox Config ----------------------------------------->
 
 # Remove splash
-echo 'disable_splash=1' >> /boot/config.txt
+sudo echo 'disable_splash=1' >> /boot/config.txt
 
 # Remove screensaver
 sudo echo '# Disable screen saver / screen blanking / power management' >> /etc/xdg/openbox/autostart
@@ -25,19 +25,19 @@ sudo echo 'xset s noblank' >> /etc/xdg/openbox/autostart
 sudo echo 'xset -dpms' >> /etc/xdg/openbox/autostart
 
 # Allow quitting the X server with CTRL-ATL-Backspace for debuging
-setxkbmap -option terminate:ctrl_alt_bksp
+sudo echo 'setxkbmap -option terminate:ctrl_alt_bksp' >> /etc/xdg/openbox/autostart
 
 
 # Start Chromium in kiosk mode
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' 	~/.config/chromium/Default/Preferences
-chromium-browser --disable-infobars --noerrdialogs --incognito --check-for-update-interval=1 --simulate-critical-update --kiosk '[https://DAKBOARD-CUSTOM-URL-HERE]'
+sudo echo 'sed -i \'s/\"exited_cleanly\":false/\"exited_cleanly\":true/\' ~/.config/chromium/\'Local State\'' >> /etc/xdg/openbox/autostart
+sudo echo 'sed -i \'s/\"exited_cleanly\":false/\"exited_cleanly\":true/; s/\"exit_type\":\"[^\"]\+\"/\"exit_type\":\"Normal\"/\' 	~/.config/chromium/Default/Preferences' >> /etc/xdg/openbox/autostart
+#sudo echo chromium-browser --disable-infobars --noerrdialogs --incognito --check-for-update-interval=1 --simulate-critical-update --kiosk '[https://DAKBOARD-CUSTOM-URL-HERE]'
 
 # Start at boot (xserver)
 sudo echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]]																		 && startx -- -nocursor' >> /home/pi/.profile
 
 # Vertical screen
-sudo echo 'display_rotate=1' >> /boot/config.txt
+#sudo echo 'display_rotate=1' >> /boot/config.txt
 
 echo Reached target: 'Auto refresh'
 sudo echo 'export display=:0,0' >> /home/pi/keyF5
@@ -46,7 +46,7 @@ sudo echo 'xdotool keydown F5; xdotool keyup F5 &' >> /home/pi/keyF5
 sudo echo 'exit' >> /home/pi/keyF5
 sudo chmod +x /home/pi/keyF5
 sudo chown pi:pi /home/pi/keyF5
-sudo -u pi (crontab -l > autoRefresh; echo "0 */120 * * * /home/pi/keyF5") | crontab -
+sudo -u pi crontab -l > autoRefresh; echo "0 */120 * * * /home/pi/keyF5" | crontab -
 rm autoRefresh
 
 # <----------------------------------------- EOF and reboot ----------------------------------------->
