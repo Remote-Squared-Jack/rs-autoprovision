5158001a84cd6fcc75bcd6207bb587aa93edab52
Remote-Squared-Jack <84894090+Remote-Squared-Jack@users.noreply.github.com>
2021-07-21 13:38:21 -0700

    Update autoprovision.sh

--- autoprovision.sh
+++ autoprovision.sh
@@ -1,57 +1,53 @@
 #!/bin/bash
 
-# <----------------------------------------- Generate Serial Number ----------------------------------------->
+# <----------------------------------------- Dakboard Requirements ----------------------------------------->
 
-FILE=/home/pi/serial
-if [[ -f "$FILE" ]]; then
-    echo "$FILE exists."
-else
-    echo Reached target: 'Get MAC address'
-    IFACE=wlan0
-    read MAC </sys/class/net/$IFACE/address
-    echo $MAC
+sudo apt-get -y update
 
-    echo Reached target: 'Grab last 4 from MAC and remove the ":"'
-    lastMAC=$(echo $MAC | awk '{print substr( $0, 13 ) }')
-    echo $lastMAC
-    echo 'Removing ":"'
-    lastMAC=$(echo $lastMAC | sed 's/://g')
-    echo 'Result:'
-    echo $lastMAC
+# Xorg requirements
+sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox -y
 
-    echo Reached target: 'Get time in ms'
-    time=$(($(date +%s%N)/1000))
+# Chromium browser install
+sudo apt-get install --no-install-recommends chromium-browser -y
 
-    echo Reached target: 'Combine lastMAC and time for serial'
-    serial=$lastMAC-$time
-    echo $serial >> /home/pi/serial.var
+# Refresh tool
+sudo apt-get install xdotool -y
 
-# <----------------------------------------- Set Hostname ----------------------------------------->
+# <----------------------------------------- Openbox Config ----------------------------------------->
 
-    echo Reached target: 'Saves the serial num to hostname file to be set on reboot'
-    echo r2d$serial > /etc/hostname
+# Remove splash
+echo 'disable_splash=1' >> /boot/config.txt
 
-# <----------------------------------------- URL Bits ----------------------------------------->
+# Remove screensaver
+sudo echo '# Disable screen saver / screen blanking / power management' >> /etc/xdg/openbox/autostart
+sudo echo 'xset s off' >> /etc/xdg/openbox/autostart
+sudo echo 'xset s noblank' >> /etc/xdg/openbox/autostart
+sudo echo 'xset -dpms' >> /etc/xdg/openbox/autostart
 
-    echo Reached target: 'Detect, read, and collect the data from /boot/URL'
+# Allow quitting the X server with CTRL-ATL-Backspace for debuging
+setxkbmap -option terminate:ctrl_alt_bksp
 
-    URL=$(cat /boot/url) # Boot url
 
-    sudo echo $'chromium-browser --disable-infobars --noerrdialogs --incognito --check-for-update-interval=1 --simulate-critical-update --kiosk \'' + $URL + ''\'' >> /etc/xdg/openbox/autostart
+# Start Chromium in kiosk mode
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' 	~/.config/chromium/Default/Preferences
+chromium-browser --disable-infobars --noerrdialogs --incognito --check-for-update-interval=1 --simulate-critical-update --kiosk '[https://DAKBOARD-CUSTOM-URL-HERE]'
 
-    echo Reached target: 'Auto refresh'
-    sudo echo 'export display=:0,0' >> /home/pi/keyF5
-    sudo echo 'xdotool keydown F5; xdotool keyup F5 &' >> /home/pi/keyF5
-#'
-    sudo echo 'exit' >> /home/pi/keyF5
-    sudo chmod +x /home/pi/keyF5
-    sudo chown pi:pi /home/pi/keyF5
-    sudo -u pi (crontab -l > autoRefresh; echo "0 */120 * * * /home/pi/keyF5") | crontab -
-    #sudo -u pi crontab -l > autoRefresh
-    #sudo -u pi echo "0 */30 * * * /home/pi/keyF5" >> autoRefresh # Refresh every 30 min
-    #sudo -u pi crontab autoRefresh
-    rm autoRefresh
-fi
+# Start at boot (xserver)
+sudo echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]]																		 && startx -- -nocursor' >> /home/pi/.profile
 
-# <----------------------------------------- EOF ----------------------------------------->
-echo Reached target: 'EOF'
+# Vertical screen
+sudo echo 'display_rotate=1' >> /boot/config.txt
+
+echo Reached target: 'Auto refresh'
+sudo echo 'export display=:0,0' >> /home/pi/keyF5
+sudo echo 'xdotool keydown F5; xdotool keyup F5 &' >> /home/pi/keyF5
+    #'
+sudo echo 'exit' >> /home/pi/keyF5
+sudo chmod +x /home/pi/keyF5
+sudo chown pi:pi /home/pi/keyF5
+sudo -u pi (crontab -l > autoRefresh; echo "0 */120 * * * /home/pi/keyF5") | crontab -
+rm autoRefresh
+
+# <----------------------------------------- EOF and reboot ----------------------------------------->
+sudo reboot
